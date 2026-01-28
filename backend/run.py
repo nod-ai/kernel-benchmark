89daba9d90c50286292b6e2752b417ae111736ff
Surya Jasper <45545431+suryajasper@users.noreply.github.com>
2026-01-27 18:22:08 -0800

    Fixed docker build and run command

--- backend/run.py
+++ backend/run.py
@@ -1,21 +1,21 @@
 import multiprocessing
-import listener
-import event_loop
-import server
+from backend.listener import serve_listener
+from backend.event_loop import serve_event_loop
+from backend.server import serve_backend
 
 
 def run_all_servers():
     processes = []
 
-    p1 = multiprocessing.Process(target=listener.serve_listener)
+    p1 = multiprocessing.Process(target=serve_listener)
     p1.start()
     processes.append(p1)
 
-    p2 = multiprocessing.Process(target=event_loop.serve_event_loop)
+    p2 = multiprocessing.Process(target=serve_event_loop)
     p2.start()
     processes.append(p2)
 
-    p3 = multiprocessing.Process(target=server.serve_backend)
+    p3 = multiprocessing.Process(target=serve_backend)
     p3.start()
     processes.append(p3)
 
